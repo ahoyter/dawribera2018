aa385c14d5639554119616dfab196f550ce217c4
OOQQ <ooqq@yahoo.com>
2018-04-17 21:27:50 +0200

    updates

--- javichu/svcUsageGreaterThan1000_script.bat
+++ javichu/svcUsageGreaterThan1000_script.bat
@@ -1,5 +1,5 @@
-echo ^<html^>^<head^>^<title^>lista de procesos svchost.exe^</title^>^</head^>^<body^>^<pre^> > usageGreaterThan1000.html
-tasklist /fi "memusage gt 10000" /fo table >> usageGreaterThan1000.html
+echo ^<html^>^<head^>^<title^>lista de procesos cuyo uso de memoria es mayor de 1000k^</title^>^</head^>^<body^>^<pre^> > usageGreaterThan1000.html
+tasklist /fi "memusage gt 1000" /fo table >> usageGreaterThan1000.html
 echo ^</pre^>^</body^>^</html^> >> usageGreaterThan1000.html
 
 @echo off
