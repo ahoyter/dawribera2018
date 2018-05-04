c23ffd19bb10c09961cd4c87bef10f614dabfb83
OOQQ <ooqq@yahoo.com>
2018-05-04 13:15:19 +0200

    Add files via upload

---
+++ javichu/listaDeUsuarios.ps1
@@ -0,0 +1,4 @@
+﻿#pedimos la lista de usuarios
+Get-LocalGroupMember "Usuarios" | ConvertTo-Html -title "Lista de Usuarios en el grupo _Usuarios_" -CssUri ".\css\style.css" -pre "<h1>Lista de Usuarios en el grupo _Usuarios_</h1>" -post "<footer><small>generado el dia 27 de Abril de 2018</small></footer>" >> listaDeUsuarios.html
+Get-LocalGroupMember "Usuarios" | ConvertTo-Csv >> listaDeUsuarios.csv
+Get-LocalGroupMember "Usuarios" | ConvertTo-Json >> listaDeUsuarios.json
