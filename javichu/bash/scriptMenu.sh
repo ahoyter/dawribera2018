a5e9c1cf60ad453b93f914998afc8b86beb10848
OOQQ <ooqq@yahoo.com>
2018-05-30 09:26:44 +0200

    Update scriptMenu.sh

--- javichu/bash/scriptMenu.sh
+++ javichu/bash/scriptMenu.sh
@@ -1,1 +1,39 @@
-a
+#!/bin/bash
+menuSel=1
+discard=0
+
+function menuFunc() {
+  echo "::menu::";
+  echo "1- usuarios";
+  echo "2- grupos";
+  echo "3- procesos";
+  echo "4- pkts instalados";
+  echo "0- salir";
+  echo "seleccione una opcion:";  
+}
+
+function displayFunc() {
+  case "$menuSel" in
+    1) cut -d: -f1 /etc/passwd
+      ;;
+    2) cut -d: -f1 /etc/group
+      ;;
+    3) ps -aux
+      ;;
+    4) dpkg -l
+      ;;
+    *) break
+      ;;
+  esac
+}
+
+while [ $menuSel -ne 0 ]; do
+  menuFunc
+  read menuSel
+  clear
+  
+  displayFunc
+  echo "presiona una tecla para continuar...";
+  read discard
+  clear
+done
