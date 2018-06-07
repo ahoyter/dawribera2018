65828a41431b8edac7aa2801d4a43264d66ba318
OOQQ <ooqq@yahoo.com>
2018-06-07 12:23:49 +0200

    Delete userProccessNotOwned_script.bat

--- javichu/userProccessNotOwned_script.bat
+++
@@ -1,8 +0,0 @@
-echo ^<html^>^<head^>^<title^>lista de procesos cuyo propietario no sea el usuario^</title^>^</head^>^<body^>^<pre^> > userProccessNotOwned.html
-tasklist /fi "services ne ooqq" /fo list >> userProccessNotOwned.html
-echo ^</pre^>^</body^>^</html^> >> userProccessNotOwned.html
-
-@echo off
-cls
-call rake
-pause
